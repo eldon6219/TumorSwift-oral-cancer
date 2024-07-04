46afa8fe3d312908418d240288cf47fa60906140
don <150234410+eldon6219@users.noreply.github.com>
2024-07-05 02:39:57 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,47 @@
+import numpy as np
+from flask import Flask , render_template , request
+from keras.preprocessing import image
+from keras.applications.efficientnet import preprocess_input
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+# from keras.applications.vgg16 import preprocess_input
+# from keras.applications.vgg16 import decode_predictions
+# from keras.applications.vgg16 import VGG16
+import tensorflow as tf
+model = tf.keras.models.load_model('models/Oral.h5')
+class_labels = ['Normal', 'Positive Oral Cancer']
+
+
+app = Flask(__name__) 
+
+@app.route('/' , methods=['GET'])
+def hello_world():
+    return render_template("index.html")
+
+@app.route('/predict' , methods=['POST'])
+def predict():
+
+    # Get the values from the form in the request
+    imagefile=request.files['imagefile']
+    image_path = "./img/" + imagefile.filename
+    imagefile.save(image_path)
+
+    # img1 = image.load_img(image_path, target_size=(224, 224,3))
+    # img_array = image.img_to_array(img1)
+    image = load_img(image_path , target_size=(224,224,3))
+    image = img_to_array(image)
+    image = np.expand_dims(image, axis=0)
+    image = preprocess_input(image)
+    # image = image.reshape((1 ,image.shape[0] , image.shape[1] , image.shape[2]))
+    # image = preprocess_input(image)
+    prediction = model.predict(image)
+    predicted_class_index = np.argmax(prediction)
+    predicted_class_label = class_labels[predicted_class_index]
+
+    # predictions = model.predict(image)
+    # result = decode_predictions(predictions)
+    # result = result[0][0]
+    return render_template("result.html" , predictions=predicted_class_label)
+
+if __name__ == '__main__':
+    app.run(debug=True)
